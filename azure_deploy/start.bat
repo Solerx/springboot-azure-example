ef2193df0865a370626c16fb9a099f8ffe930c3e
adaofeliz <adao@jimmyboys.pt>
2015-05-07 14:45:30 +0100

    Generated artifact is now a jar

--- azure_deploy/start.bat
+++ azure_deploy/start.bat
@@ -1,5 +1,5 @@
 set "JAVA_HOME=d:\Program Files\Java\jdk1.8.0_25\bin\java.exe"
-set "CMD_LINE_ARGS=-Djava.net.preferIPv4Stack=true -Dserver.port=%HTTP_PLATFORM_PORT% -jar d:\home\site\wwwroot\springboot-azure-example-deploy.war"
+set "CMD_LINE_ARGS=-Djava.net.preferIPv4Stack=true -Dserver.port=%HTTP_PLATFORM_PORT% -jar d:\home\site\wwwroot\springboot-azure-example-deploy.jar"
 set "CMD=%JAVA_HOME%\bin\java.exe %CMD_LINE_ARGS%"
 echo Using JAVA_HOME:       "%JAVA_HOME%"
 echo Using CMD_LINE_ARGS:       "%CMD_LINE_ARGS%"
