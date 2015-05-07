84e84ec0df5a1e79383638e64dd8f8fcddc1c7ab
adaofeliz <adao@jimmyboys.pt>
2015-05-07 10:51:36 +0100

    Enabling Support for Azure Web Apps deployment via FTP.

--- src/main/java/pt/jimmyboys/azuresample/Application.java
+++ src/main/java/pt/jimmyboys/azuresample/Application.java
@@ -3,12 +3,14 @@ package pt.jimmyboys.azuresample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 /**
  * Startup class that is used by spring-boot
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     /**
      * Method that is responsible for the application startup
@@ -17,6 +19,11 @@ public class Application {
      */
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 
 }
