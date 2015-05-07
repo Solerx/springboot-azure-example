335c7b62ce737681b133dda60b42d4728081f27b
adaofeliz <adao@jimmyboys.pt>
2015-05-07 14:49:02 +0100

    Fixing JAVA_HOME set

--- azure_deploy/start.bat
+++ azure_deploy/start.bat
@@ -1,4 +1,4 @@
-set "JAVA_HOME=d:\Program Files\Java\jdk1.8.0_25\bin\java.exe"
+set "JAVA_HOME=d:\Program Files\Java\jdk1.8.0_25"
 set "CMD_LINE_ARGS=-Djava.net.preferIPv4Stack=true -Dserver.port=%HTTP_PLATFORM_PORT% -jar d:\home\site\wwwroot\springboot-azure-example-deploy.jar"
 set "CMD=%JAVA_HOME%\bin\java.exe %CMD_LINE_ARGS%"
 echo Using JAVA_HOME:       "%JAVA_HOME%"
